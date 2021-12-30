645cbb72aec5737115a089abd79fdb6dee3a94ee
pmular <91500217+pmular@users.noreply.github.com>
2021-12-30 17:15:57 +0100

    final

--- raw_autoencoder.py
+++ raw_autoencoder.py
@@ -25,25 +25,25 @@ from gpflow.ci_utils import ci_niter
 ################
 # Preprocessing
 ################
-def load_data(dataset):
+def load_data(dataset, filepath):
     if dataset == "H9":
         cell_line = "H9"
-        raw_Y = pd.read_pickle('/home/pau/Desktop/MASTER/Statistical_computation/project4/data/McDavid/h9_df.pkl').T
-        cpt = pd.read_pickle('/home/pau/Desktop/MASTER/Statistical_computation/project4/data/McDavid/h9_cpt.pkl').values
+        raw_Y = pd.read_pickle(filepath+'/h9_df.pkl').T
+        cpt = pd.read_pickle(filepath+'/h9_cpt.pkl').values
         print("DATASET: ", cell_line)
         print("Original dimesion %d cells x %d genes." % raw_Y.shape)
         print(f"G0/G1 {sum(cpt == 'g0/g1')}, S {sum(cpt == 's')}, G2/M {sum(cpt == 'g2/m')}")
     elif dataset == "mb":
         cell_line = "mb"
-        raw_Y = pd.read_pickle('/home/pau/Desktop/MASTER/Statistical_computation/project4/data/McDavid/mb_df.pkl').T
-        cpt = pd.read_pickle('/home/pau/Desktop/MASTER/Statistical_computation/project4/data/McDavid/mb_cpt.pkl').values
+        raw_Y = pd.read_pickle(filepath+'/mb_df.pkl').T
+        cpt = pd.read_pickle(filepath+'/mb_cpt.pkl').values
         print("DATASET: ", cell_line)
         print("Original dimesion %d cells x %d genes." % raw_Y.shape)
         print(f"G0/G1 {sum(cpt == 'g0/g1')}, S {sum(cpt == 's')}, G2/M {sum(cpt == 'g2/m')}")
     elif dataset == "pc3":
         cell_line = "pc3"
-        raw_Y = pd.read_pickle('/home/pau/Desktop/MASTER/Statistical_computation/project4/data/McDavid/pc3_df.pkl').T
-        cpt = pd.read_pickle('/home/pau/Desktop/MASTER/Statistical_computation/project4/data/McDavid/pc3_cpt.pkl').values
+        raw_Y = pd.read_pickle(filepath+'/pc3_df.pkl').T
+        cpt = pd.read_pickle(filepath+'/pc3_cpt.pkl').values
         print("DATASET: ", cell_line)
         print("Original dimesion %d cells x %d genes." % raw_Y.shape)
         print(f"G0/G1 {sum(cpt == 'g0/g1')}, S {sum(cpt == 's')}, G2/M {sum(cpt == 'g2/m')}")
@@ -52,12 +52,12 @@ def load_data(dataset):
     
     return raw_Y, cpt
 
-seed_value = 10        
-# Load data & Transform it to numpy
-dataset = "mb" 
-data, cpt = load_data(dataset)
+
+dataset = "pc3" 
+filepath = '/Statistical_computation/project4/data/McDavid'
+data, cpt = load_data(dataset, filepath)
 data = data.to_numpy()
-data = preprocessing.scale(data)
+seed_value = 10        
 
 ###############
 # MODEL
@@ -71,6 +71,8 @@ def circular_unit(name, comp = 2):
         elif comp == 2:
             out = [keras.layers.Lambda(lambda x: keras.backend.sin(x), name=name + '_sin')(x),
                    keras.layers.Lambda(lambda x: keras.backend.cos(x), name=name + '_cos')(x)]
+            # out = [keras.layers.Lambda(lambda x: keras.backend.pow(keras.backend.sin(x), 2), name=name + '_sin')(x),
+            #        keras.layers.Lambda(lambda x: keras.backend.pow(keras.backend.cos(x), 2), name=name + '_cos')(x)]
         else:
             out = [
                 keras.layers.Lambda(lambda x: keras.backend.sin(x + 2 * pi * i / comp), name=name + '_' + str(i))(x)
@@ -234,7 +236,9 @@ y_hat = decoder('decoder', input_width)(chest)
 model = keras.Model(outputs=y_hat, inputs=y)
 model.compile(loss='mean_squared_error',
                            optimizer=keras.optimizers.Adam(rate))
+
 model.fit(data, data, epochs=1000, verbose=1)
+
 
 pseudotime = keras.backend.function(inputs=[model.get_layer('input').input],
                                      outputs=[model.get_layer('encoder_out').output]
@@ -249,10 +253,6 @@ correct_prob = cyclum.evaluation.precision_estimate([distr_g0g1, distr_s, distr_
 dis_score = correct_prob
 print("Score: ", dis_score)
 
-
-import cyclum.illustration
-color_map = {"g0/g1": "red", "s": "green", "g2/m": "blue"}
-cyclum.illustration.plot_round_distr_color(pseudotime[:, 0], cpt.squeeze(), color_map)
 ################################
 # DHLA 
 ###############################
@@ -364,17 +364,17 @@ tuner = kt.Hyperband(hypermodel = model_builder,
 tuner.search_space_summary() 
 tuner.search(data, data)
 best_hp=tuner.get_best_hyperparameters()[0]
-h_model = tuner.hypermodel.build(best_hp)
-h_model.summary()
-h_model.fit(data, data)
-pseudotime = keras.backend.function(inputs=[h_model.get_layer('input').input],
-                                     outputs=[h_model.get_layer('encoder_out').output]
-                                     )([data])[0]
-flat_embedding = (pseudotime % (2 * np.pi)) / 2
-width = 3.14 / 100 / 2;
-discrete_time, distr_g0g1 = cyclum.evaluation.periodic_parzen_estimate(flat_embedding[np.squeeze(cpt)=='g0/g1', 0])
-discrete_time, distr_s = cyclum.evaluation.periodic_parzen_estimate(flat_embedding[np.squeeze(cpt)=='s', 0])
-discrete_time, distr_g2m = cyclum.evaluation.periodic_parzen_estimate(flat_embedding[np.squeeze(cpt)=='g2/m', 0])
-correct_prob = cyclum.evaluation.precision_estimate([distr_g0g1, distr_s, distr_g2m], cpt, ['g0/g1', 's', 'g2/m'])
-dis_score = correct_prob
-print("Score: ", dis_score)
+# h_model = tuner.hypermodel.build(best_hp)
+# h_model.summary()
+# h_model.fit(data, data)
+# pseudotime = keras.backend.function(inputs=[h_model.get_layer('input').input],
+#                                      outputs=[h_model.get_layer('encoder_out').output]
+#                                      )([data])[0]
+# flat_embedding = (pseudotime % (2 * np.pi)) / 2
+# width = 3.14 / 100 / 2;
+# discrete_time, distr_g0g1 = cyclum.evaluation.periodic_parzen_estimate(flat_embedding[np.squeeze(cpt)=='g0/g1', 0])
+# discrete_time, distr_s = cyclum.evaluation.periodic_parzen_estimate(flat_embedding[np.squeeze(cpt)=='s', 0])
+# discrete_time, distr_g2m = cyclum.evaluation.periodic_parzen_estimate(flat_embedding[np.squeeze(cpt)=='g2/m', 0])
+# correct_prob = cyclum.evaluation.precision_estimate([distr_g0g1, distr_s, distr_g2m], cpt, ['g0/g1', 's', 'g2/m'])
+# dis_score = correct_prob
+# print("Score: ", dis_score)
